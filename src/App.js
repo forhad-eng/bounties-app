93e08d84271e27616f58dbd0c465e5f9c626dbd1
forhad-eng <forhadu.cse@gmail.com>
2022-06-18 23:13:07 +0600

    all tasks and columns are setup in the state and one be one column rendering process done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,102 @@
+import { useState } from 'react'
+import { DragDropContext } from 'react-beautiful-dnd'
+import Bounties from './components/Bounties/Bounties'
+
 function App() {
+    const [state, setState] = useState(initialState)
+
+    const onDragEnd = result => {
+        const { destination, source } = result
+    }
+
     return (
-        <div>
-            <h1>Welcome to bounty</h1>
+        <div className="bg-[#100E2E] rounded-lg">
+            <h2 className="text-4xl text-white text-center font-bold border-b-2 border-[#4A4A4A] mx-6 pt-10 pb-1">
+                Bounties
+            </h2>
+            <DragDropContext onDragEnd={onDragEnd}>
+                {state.columnOrder.map(columnId => {
+                    const column = state.columns[columnId]
+                    const tasks = column.taskIds.map(taskId => state.tasks[taskId])
+                    return <Bounties key={column.id} column={column} tasks={tasks} />
+                })}
+            </DragDropContext>
         </div>
     )
+}
+
+const initialState = {
+    tasks: {
+        1: {
+            id: 1,
+            taskDes: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, non.'
+        },
+        2: {
+            id: 2,
+            taskDes: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, non.'
+        },
+        3: {
+            id: 3,
+            taskDes: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, non.'
+        },
+        4: {
+            id: 4,
+            taskDes: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, non.'
+        },
+        5: {
+            id: 5,
+            taskDes: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, non.'
+        },
+        6: {
+            id: 6,
+            taskDes: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, non.'
+        },
+        7: {
+            id: 1,
+            taskDes: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, non.'
+        },
+        8: {
+            id: 2,
+            taskDes: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, non.'
+        },
+        9: {
+            id: 3,
+            taskDes: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, non.'
+        },
+        10: {
+            id: 4,
+            taskDes: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, non.'
+        },
+        11: {
+            id: 5,
+            taskDes: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, non.'
+        },
+        12: {
+            id: 6,
+            taskDes: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, non.'
+        }
+    },
+
+    columns: {
+        'column-1': {
+            colId: 'column-1',
+            taskIds: [1, 2, 3]
+        },
+        'column-2': {
+            colId: 'column-2',
+            taskIds: [4, 5, 6]
+        },
+        'column-3': {
+            colId: 'column-3',
+            taskIds: [7, 8, 9]
+        },
+        'column-4': {
+            colId: 'column-4',
+            taskIds: [10, 11, 12]
+        }
+    },
+
+    columnOrder: ['column-1', 'column-2', 'column-3']
 }
 
 export default App
---
+++ src/components/Bounties/Bounties.js
@@ -0,0 +1,10 @@
+const Bounties = ({ column, tasks }) => {
+    console.log(tasks)
+    return (
+        <div>
+            <p>Hello</p>
+        </div>
+    )
+}
+
+export default Bounties
